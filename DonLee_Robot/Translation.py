87f078d81a763e16f682609bafdfc3c40290424b
jithinsdileep12 <89510784+jithinsdileep12@users.noreply.github.com>
2021-12-23 21:02:19 +0530

    Update Translation.py

--- DonLee_Robot/Translation.py
+++ DonLee_Robot/Translation.py
@@ -5,10 +5,10 @@
 class Translation(object):
     
     START_TEXT = """<b>🙋‍♂️Hey {}!!</b>
-<b>Am Just A Donlee Adv Auto Filter Bot....😉</b>
+<b>Am Just A Filter Bot....😉</b>
 
-<b>Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝</b>
-<b>Watch the video below to build an DonLee Adv Autofilter bot like me...</b>
+<b>don't add me to your group plz</b>
+<b>Adv Autofilter bot like me...</b>
 <b>Subscribe to the update channel to learn about my updates and activity...</b>
 <b>Press /help to know about available commands🤪</b>"""    
    
@@ -47,11 +47,11 @@ class Translation(object):
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/Mrk_YT">Muhammed Rk</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/kjmods"kj mods</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/PR0FESS0R-99/DonLee_Robot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/jithinsdileep12">Click Me</a></i>
 """
